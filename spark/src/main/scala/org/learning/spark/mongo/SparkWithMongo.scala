354db63c6d0faef7aed0f7345d289bfc798e3163
Jeffrey Chen <support-jec@hotmail.com>
2017-03-27 15:08:00 +0800

    spark-mongo: readconfig

--- spark/src/main/scala/org/learning/spark/mongo/SparkWithMongo.scala
+++ spark/src/main/scala/org/learning/spark/mongo/SparkWithMongo.scala
@@ -1,9 +1,17 @@
 package org.learning.spark.mongo
 
 import org.apache.spark.sql.SparkSession
+import com.mongodb.spark._
+import com.mongodb.spark.config._
+import org.bson.Document
 
 class SparkWithMongo (private val spark : SparkSession) {
+    import spark.imlicits._
+
     def readPlainDocument : Unit = {
+        val readConfig = ReadConfig(
+            Map("uri" -> "mongodb://user:pass@node1:27017,node2:27017,node3:27017/database.collection?replicaSet=rpSetName"))
+        val collectionDS = MongoSpark.load(spark, readConfig)
     }
     def readComplexDocument : Unit = {
     }
