a2b0ec27b0039a4109019f4ab3f84773cfb6f78d
Jeffrey Chen <support-jec@hotmail.com>
2017-04-01 15:05:33 +0800

    scala-basics: add project

---
+++ scala/src/main/scala/org/learning/progscala/typelessdomore/simicolon-example.scala
@@ -0,0 +1,10 @@
+def equalsign (s : String) = 
+    println("equalsign: " + s)
+
+def equalsign2 (s : String) = {
+    println("equalsign2: " + 2)
+}
+
+def commas (s1 : String, s2 : String) = {
+    println("comma: " + s1 + ", " + s2)
+}
