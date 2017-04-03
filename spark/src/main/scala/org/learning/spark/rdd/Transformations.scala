0dbffb555ea5ec840622c86b64ad5a83cefc8c1a
Jeffrey Chen <support-jec@hotmail.com>
2017-04-03 17:41:58 +0800

    spark-rdd: map partitions

--- spark/src/main/scala/org/learning/spark/rdd/Transformations.scala
+++ spark/src/main/scala/org/learning/spark/rdd/Transformations.scala
@@ -15,4 +15,15 @@ class Transformations (sc : SparkContext) {
         rdd.flatMap ( i => i.split(',') )
         rdd.flatMap { _.split(',') }
     }
+
+    def mapPartitions () = {
+        val data = Array(1,2,3,4,5,6)
+        val rdd = sc.parallelize(data, 3)
+        val newRdd = rdd.mapPartitions { iter =>
+            val castIter : InterruptibleIterator[Int] = iter.asInstanceOf[InterruptibleIterator[Int]]
+            var partitionId = castIter.context.partitionId
+            iter.map (i => partitionId * i)
+        }
+        newRdd.foreach(println)
+    }
 }
