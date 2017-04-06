5b414e063888e6ad3bb0ed454a943d1ee1ca7b97
Jeffrey Chen <support-jec@hotmail.com>
2017-04-06 22:03:00 +0800

    shell-basic: cp cmd

--- shell/basic-bash-shell-commands.sh
+++ shell/basic-bash-shell-commands.sh
@@ -30,3 +30,21 @@ touch test_one
 # change the access time
 touch -a test_one
 
+# copy files
+cp test_one test_two
+
+# copy files - force confirm overwrite
+cp -i test_one test_two
+
+# copy file to an existing directory
+cp test_one ~/Documents/
+
+# recursively copy
+cp -R scripts/ Mod_scripts
+
+# wildcard match copy
+cp *script Mod_scripts/
+
+# Link files
+# create a symbolic to a file
+ln -s data_file sl_data_file
